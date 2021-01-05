36fd9d5ccc607aefe81dcb3720478201991b56cd
Javed Hussain <jdhn42@gmail.com>
2021-01-05 08:46:22 -0500

    updated description

--- src/mock/data.js
+++ src/mock/data.js
@@ -4,7 +4,7 @@ import { nanoid } from 'nanoid';
 export const headData = {
   title: 'Portfolio', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  description: 'Javed\'s Portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
