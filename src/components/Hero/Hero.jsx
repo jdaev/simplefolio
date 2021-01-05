cd98962e58bd341fed33fc97006c89d027750255
Javed Hussain <jdhn42@gmail.com>
2021-01-04 19:06:17 -0500

    data update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,10 +26,10 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            {title || 'Hi, I\'m'}{' '}
+            <span className="text-color-main">{name || 'Javed'}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || 'I can make websites, apps and games for your needs.'}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
